a014d3248c71ea385cab6ac8918f512f09d25262
Patrick Li <patrickli727@gmail.com>
2016-03-20 15:49:24 -0700

    nlogn sorting algorithms

--- ruby.rb
+++ ruby.rb
@@ -1,3 +1,50 @@
-def bsearch (arr, target)
+require 'byebug'
 
+def bsearch (arr, target)
+  return nil if arr.empty?
+
+  median = arr.length / 2
+
+  case target <=> arr[median]
+  when -1
+    bsearch(arr[0...median], target)
+  when 0
+    return median
+  when 1
+    pos = bsearch(arr[median + 1 .. -1], target)
+    return nil if pos.nil?
+    pos + median + 1
+  end
 end
+
+# puts bsearch([1,2,3,3,5,7], 5)
+
+def merge_sort (arr, &prc)
+  prc ||= Proc.new { |x, y| x <=> y }
+
+  return arr if arr.count <= 1
+
+  median = arr.length / 2
+
+  left = arr[0...median]
+  right = arr[median .. -1]
+
+  merge(merge_sort(left, &prc), merge_sort(right, &prc), &prc)
+end
+
+def merge (left, right, &prc)
+  result = []
+
+  until left.empty? || right.empty?
+    case prc.call(left.first, right.first)
+    when -1, 0
+      result << left.shift
+    when 1
+      result << right.shift
+    end
+  end
+
+  result.concat(left).concat(right)
+end
+
+# p merge_sort([5,3,7,1,2]) { |x, y| y <=> x }
