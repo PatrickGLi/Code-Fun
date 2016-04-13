9a00169019f6604004833b370b45f2b9939c1fda
Patrick Li <patrickli727@gmail.com>
2016-04-13 15:43:08 -0700

    added javascript pair sum.

--- speed_test/test.js
+++ speed_test/test.js
@@ -54,3 +54,45 @@ function largestContiguousSum(array) {
 };
 
 // console.log(largestContiguousSum([4,3,7,1,-1,-5,4,-15,20]))
+
+function sillyYears(year) {
+  var results = [];
+
+  while (results.length < 10) {
+    year ++;
+    var stringYear = year.toString();
+    var first = parseInt(stringYear.substring(0, 2)),
+        second = parseInt(stringYear.substring(2, 4)),
+        middle = parseInt(stringYear.substring(1, 3));
+
+    if (first + second === middle) {
+      results.push(year);
+    }
+  }
+
+  return results;
+};
+
+// console.log(sillyYears(1990));
+
+function pairSum(arr, k) {
+  var seen = new Set();
+  var pairs = new Set();
+
+  arr.forEach(function(el) {
+    if (seen.has(el)) {
+      return;
+    }
+
+    if (seen.has(k - el)) {
+      var pair = [Math.min(k - el, el), Math.max(k - el, el)];
+      pairs.add(pair);
+    }
+
+    seen.add(el);
+  });
+
+  return pairs;
+}
+
+// console.log(pairSum([1,5,3,4,6,3], 4));
