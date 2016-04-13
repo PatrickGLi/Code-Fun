9d153d97eec7f5206078d09950e0b91e8b192ac0
Patrick Li <patrickli727@gmail.com>
2016-04-13 15:22:07 -0700

    added javascript solutions.

--- speed_test/test.js
+++ speed_test/test.js
@@ -18,3 +18,39 @@ Array.prototype.myMap = function(cb) {
 
   return result;
 };
+
+function uniqueSubs(string) {
+  if (string.length == 0) {
+    return [[]];
+  }
+
+  var previous = uniqueSubs(string.substring(1, string.length));
+  var new_elements = previous.map(function(el){
+    return string[0] + el;
+  });
+
+  return previous.concat(new_elements);
+
+};
+
+// console.log(uniqueSubs("hey"));
+
+function largestContiguousSum(array) {
+  var largestSum = null;
+
+  array.forEach(function(element) {
+    if (typeof largestSum === "null") {
+      largestSum = element;
+      return;
+    }
+
+    largestSum += element;
+    if (largestSum < element) {
+      largestSum = element;
+    }
+  });
+
+  return largestSum;
+};
+
+// console.log(largestContiguousSum([4,3,7,1,-1,-5,4,-15,20]))
